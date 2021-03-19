48f10b7f1aaaa0d3c1cb6c303a0a2655f2af724b
johnyjey <johnjpalson@gmail.com>
2021-03-19 21:38:45 +0530

    Accessing the current logged user and logout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,10 +3,10 @@ class MainController < ApplicationController
         # flash[:notice] ="Logged in successfully!"
         # flash[:alert] ="Email or password mismatched"
 
-        if session[:user_id]
-            #@user =User.find(session[:user_id])
-            @user =User.find_by(id: session[:user_id])
-        end
+        # if session[:user_id]
+        #     #@user =User.find(session[:user_id])
+        #     @user =User.find_by(id: session[:user_id])
+        # end
     end
 end
 
