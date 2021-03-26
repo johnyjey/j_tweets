7900800e7300ea9f5e41c4aa3f8cec91c12e61ee
johnyjey <johnjpalson@gmail.com>
2021-03-26 19:55:35 +0530

    Call a form, Tweet edit and destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
     before_action :require_user_logged_in!
+    before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
     def index
        @tweets = Current.user.tweets 
     end
@@ -18,9 +20,31 @@ class TweetsController < ApplicationController
         end
     end
 
+    def edit
+        
+    end
+
+    def update
+        if @tweet.update(tweet_params)
+            redirect_to tweets_path, notice: "Tweet was updated successfully!"
+        else
+            render :edit
+        end        
+    end
+
+    def destroy
+        @tweet.destroy
+        redirect_to tweets_path, notice: "Tweet was unscheduled successfully!"
+    end
+
+    ## Functions 
     private 
     def tweet_params
         params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
     end
 
+    # Function for set tweet
+    def set_tweet
+        @tweet = Current.user.tweets.find(params[:id])
+    end
 end
