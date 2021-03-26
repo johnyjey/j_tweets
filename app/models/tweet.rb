a211f8c8fc9297ade8d757186e3dd6f48dcd1ecd
johnyjey <johnjpalson@gmail.com>
2021-03-26 21:15:28 +0530

    Using Twitter API & BG job for posting tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,14 +1,25 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
-  validates :body, length: {minimum: 1, maximum: 280}
-  validates :publish_at, presence: true
+  validates :body, length: {minimum: 1, maximum: 280} # Validation : Minimum and Maximum length
+  validates :publish_at, presence: true               # Required
   
   after_initialize do
     self.publish_at ||=24.hour.from_now
   end
 
+  after_Save_commit do
+    if publish_at_previously_changed?       
+      TweetJob.set(wait_until: publish_at).perform_later(self)
+    end
+  end
+
   def published?
     tweet_id?
+  end
+
+  def publish_to_twitter!
+    tweet = twitter_account.client.update(body)
+    update(tweet_id: tweet.id)
   end
 end
